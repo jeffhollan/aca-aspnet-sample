7303b7cd566469611bb9c99c25149da705005e97
Jeff Hollan <jeffhollan@outlook.com>
2021-11-22 15:34:59 -0800

    adding app insights

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
